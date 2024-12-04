bb0a36a8671d2acdd4158fffc9bd15d3bc1639a5
JonBonda <jbonda@my.centennialcollege.ca>
2024-12-04 14:36:31 -0700

    Pre-Alpha
    Initial build after background&animations but still working on playercontroller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Reference Youtube Source: BMo
+// 2D Side Scroller MOVEMENT in Unity (BEGINNER FRIENDLY)
+// https://www.youtube.com/watch?v=9HAZQROH2gM
+// https://www.youtube.com/playlist?list=PLhrayuv80FeWPrco0bqfYSHDifVB2PXpW
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 20.0f;
+
+    private Rigidbody2D rb;
+    public Animator animator;
+    private string currentAnimation = "";
+
+    private bool facingRight = true;
+    private float moveDirection;
+
+    public float jumpForce = 15.0f;
+    private bool isJumping = false;
+    private int jumpCount;
+    public int maxJumpCount = 3;
+
+    public Transform ceilingCheck;
+    public Transform groundCheck;
+
+    public LayerMask groundObject;
+    public bool isGrounded;
+    public float checkRadius;
+
+
+
+
+    public void Start()
+    {
+        jumpCount = maxJumpCount;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+
+    }
+
+    private void ChangeAnimation(string animation, float crossfaderate = 0.2f)
+    {
+        if (currentAnimation != animation)
+        {
+
+            // animator.Play();
+            currentAnimation = animation;
+            animator.CrossFade(animation, crossfaderate);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Get Inputs
+        ProcessInput();
+
+        // Animate If FlipCharacter 180 for RightLeft
+        Animate();
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObject);
+        // if (isGrounded)        {            jumpCount = maxJumpCount;         }
+
+        // Move
+        Move();
+
+    }
+
+    // Move to new direction
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping)
+        {
+            // rb.AddForce(new Vector2(0f, jumpForce));
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            // jumpCount--;
+        }
+        isJumping = false;
+    }
+
+    // Flip/Rotate 180 Character Left/Right Right/Left
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            flipCharacter();
+            animator.CrossFade("Run", 0);
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            flipCharacter();
+            animator.CrossFade("Run", 0);
+            
+        }
+        else if (moveDirection == 0)
+        {
+            animator.CrossFade("Idle", 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.R) && moveDirection != 0) //  DASH
+        {
+            animator.CrossFade("Dash", 0);
+        }
+
+
+        //else if (moveDirection == 0 && Input.GetKeyDown(KeyCode.DownArrow))   animator.CrossFade("MMX_Crouch", 0);
+
+
+
+    }
+
+    private void ProcessInput()
+    {
+        // Get Imputs
+        moveDirection = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") )// && jumpCount > 0) // if (Input.GetButtonDown("Jump")  && isGrounded) // if (Input.GetKey(KeyCode.Space) && isGrounded) 
+        {
+            isJumping = true;
+            Debug.Log("Spacebar JUMP");
+            Debug.Log("JumpCount#: " + jumpCount);
+            animator.CrossFade("Jump", 0);
+        }
+
+    }
+
+    private void flipCharacter()
+    {
+        facingRight = !facingRight;
+        rb.transform.Rotate(0f, 180f, 0f);
+    }
+
+}
